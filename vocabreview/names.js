3b11404e1d290049eba0f5b24c630a1673fc2ff5
BIPHMusic <135597439+BIPHMusic@users.noreply.github.com>
2025-03-22 13:04:37 +0800

    Add files via upload

---
+++ vocabreview/names.js
@@ -0,0 +1,8 @@
+const approvedNames = [
+  "ben chen", "howard ding", "terry he", "alice li", "vincent mu",
+  "tiger pan", "chris peng", "lucy sheng", "diego su", "hailey wang",
+  "eileen xu", "angelina yan", "serena zeng", "catherine zhang",
+  "andrew zhao", "jennifer zhou", "ben", "howard", "terry", "alice",
+  "vincent", "tiger", "chris", "lucy", "diego", "hailey", "eileen",
+  "angelina", "serena", "catherine", "andrew", "jennifer", "mr. tubbs"
+];
